ce6f24c608419e07ebf0255fd8584adb0af4459c
KricejJanezMartin <jkricej@example.com>
2024-02-29 15:09:18 +0100

    initial project setup and start

---
+++ my-portfolio/src/components/Header.svelte
@@ -0,0 +1,29 @@
+<script> 
+    export let y; //pošiljanje 
+
+    let tabs = [        
+        {name: "Projects", link:"#projects"},
+        {name: "About me", link: "#about"},
+       // {name: "Contact", link: "#contact"},
+       // {name: "Blog", link: "#blog"}
+        ]
+</script> 
+
+<header class= {"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border  border-solid" + (y > 0 ? "py-4 bg-slate-950 border-violet-950": "py-6 bg-transparent border-transparent")}>
+<h1 class="font-medium">
+    <b class="font-bold poppins">Janez Martin</b> Kričej
+</h1>
+
+<div class="sm:flex items-center gap-4 hidden">
+    {#each tabs as tab, index}
+    <a href={tab.link} class="duration-200 hover:text-violet-400">
+        <p>{tab.name}</p>
+    </a>
+    {/each}
+
+
+</div>
+
+
+
+</header>
