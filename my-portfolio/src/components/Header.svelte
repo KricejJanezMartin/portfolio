1409adf9f21ec69f558521fa8ae60659538dab98
KricejJanezMartin <jkricej@example.com>
2024-03-04 15:29:01 +0100

    step and main component

--- my-portfolio/src/components/Header.svelte
+++ my-portfolio/src/components/Header.svelte
@@ -4,7 +4,7 @@
     let tabs = [        
         {name: "Projects", link:"#projects"},
         {name: "About me", link: "#about"},
-       // {name: "Contact", link: "#contact"},
+        {name: "Contact", link: "#contact"},
        // {name: "Blog", link: "#blog"}
         ]
 </script> 
@@ -20,9 +20,17 @@
         <p>{tab.name}</p>
     </a>
     {/each}
-
-
 </div>
+
+
+<button
+class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+>
+<div
+    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+/>
+<h4 class="relative z-9">Get in touch</h4>
+</button>
 
 
 
